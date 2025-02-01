8d35f1d92a75d47f8e3389801769861a3f9f8346
Jose Alberto Chaparro Castro <jachaparro@unicesar.edu.co>
2025-01-31 22:16:43 -0500

    Dashboard responsive

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -135,7 +135,7 @@ const AdminDashboard = () => {
       </h3>
       
       {/* Tabla para pantallas medianas y grandes */}
-      <div className="hiddenoverflow-x-auto bg-white dark:bg-gray-800 rounded-lg">
+      <div className="hidden md:block overflow-x-auto bg-white dark:bg-gray-800 rounded-lg">
       <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
         <thead className="bg-gray-50 dark:bg-gray-900">
           <tr>
